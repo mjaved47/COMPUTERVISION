f5d62d3a32e23f34c49c168f08bf741164e613ff
mjaved47 <emailofjaved@gmail.com>
2022-10-30 19:55:30 +0300

    Basics including reading image, getting dimensions, resizing, and conversion

--- basics/cvRead.py
+++ basics/cvRead.py
@@ -2,10 +2,23 @@ import cv2 as cv
 
 # reading an image
 dog = cv.imread('images\happydog.jpg')
-cv.imshow('Dog', dog)
+
+# getting size of an image
+height = dog.shape[0]
+width = dog.shape[1]
+print('height =' + str(height))
+print('width =' + str(width))
+cv.imshow('Dog', dog)                    #---------------------showing the image in its original dimensions   
+
+# resizing an image
+smallSize = (int(width/4), int(height/4))#---------------------resizing the original image to the quarter of its original size.
+dogSmall = cv.resize(dog, smallSize, cv.INTER_LINEAR )
+cv.imshow('Resize', dogSmall)
 
 # converting an image to grayscale
 dogGrayscale = cv.cvtColor(dog, cv.COLOR_BGR2GRAY)
-cv.imshow('Grayscale', dogGrayscale)
+# cv.imshow('Grayscale', dogGrayscale)
+
+
 
 cv.waitKey(0)
