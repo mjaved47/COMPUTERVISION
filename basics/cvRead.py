af80171e5363f27e91dd7cc3cdf1f4fbaf689b22
mjaved47 <emailofjaved@gmail.com>
2022-10-30 20:42:27 +0300

    Updated resizing with different interpolation

--- basics/cvRead.py
+++ basics/cvRead.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+cv.destroyAllWindows()
 
 # reading an image
 dog = cv.imread('images\happydog.jpg')
@@ -8,12 +9,15 @@ height = dog.shape[0]
 width = dog.shape[1]
 print('height =' + str(height))
 print('width =' + str(width))
-cv.imshow('Dog', dog)                    #---------------------showing the image in its original dimensions   
+# cv.imshow('Dog', dog)#--------------------------------------------showing the image in its original dimensions   
 
 # resizing an image
-smallSize = (int(width/4), int(height/4))#---------------------resizing the original image to the quarter of its original size.
-dogSmall = cv.resize(dog, smallSize, cv.INTER_LINEAR )
-cv.imshow('Resize', dogSmall)
+small_size = (int(width/4), int(height/4))#-----------------------resizing the original image to the quarter of its original size.
+dog_small_linear = cv.resize(dog, small_size, cv.INTER_LINEAR)#---LINEAR Interpolation
+cv.imshow('Resize Linear Interpolation', dog_small_linear)
+
+dog_small_cubic = cv.resize(dog, small_size, cv.INTER_CUBIC)#------CUBIC Interpolation
+cv.imshow('Resize Cubic Interpolation', dog_small_cubic)
 
 # converting an image to grayscale
 dogGrayscale = cv.cvtColor(dog, cv.COLOR_BGR2GRAY)
